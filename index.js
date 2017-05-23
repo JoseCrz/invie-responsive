9c7d83c72ac36b2c0189d655037fdb5b60e4017f
Jose Cuevas <cueramjose@gmail.com>
2017-05-23 11:29:24 -0500

    Agregado media queries con JS

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 var burgerButton = document.getElementById("burger-button")
 var menu = document.getElementById("menu")
+var consulta = window.matchMedia("(max-width: 500px)")
+consulta.addListener(mediaQuery)
 
-burgerButton.addEventListener("touchstart", toggleMenu)
+
+function mediaQuery () {
+  if (consulta.matches) {
+    console.log("Se cumplió la condición")
+    burgerButton.addEventListener("touchstart", toggleMenu)
+  } else {
+    console.log("No se cumplió la condición")
+    burgerButton.removeEventListener("touchstart", toggleMenu)
+  }
+}
 
 function toggleMenu () {
   menu.classList.toggle("active")
 }
+
+mediaQuery()
